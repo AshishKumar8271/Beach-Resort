4e6c20d0b5b104a80677da1e6e59dce79be1e253
Ashish <ashishgujjar277@gmail.com>
2023-09-29 00:02:33 +0530

    Beach Resort Home page Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import FeatureRoom from '../Components/Home/FeatureRoom';
 const Home = () => {
   return (
     <>
-    <Banner title="Luxurious Rooms" des="Delux Rooms starting at $299" link="/rooms/" imgLink="./images/defaultBcg.jpeg"/>
+    <Banner title="Luxurious Rooms" des="Delux Rooms starting at $299" link="/rooms/" imgLink="https://img.iproperty.com.my/angel-legacy/1110x624-crop/static/2021/04/Mengenal-Resort-Karakteristik-Jenis-dan-5-Rekomendasi-Resort-Terbaik.png"/>
     <Services/>
     <FeatureRoom/>
     </>
