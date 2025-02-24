a41e792d592cf3771496e981ee0dea5dc3d008f9
AshishKumar8271 <ashishgujjar277@gmail.com>
2025-02-24 13:24:24 +0530

    removed context

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Rooms from './pages/Rooms';
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
-import { UseCustomHook } from './Context';
 import {Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Home/Navbar';
 import { useLocation } from 'react-router-dom';
